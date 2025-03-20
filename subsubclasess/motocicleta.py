654c4c634483e99988417618f586217ddde56c41
XBruno1 <pazgerald54@gmail.com>
2025-03-20 12:18:48 +0100

    en progreso

---
+++ subsubclasess/motocicleta.py
@@ -0,0 +1,5 @@
+from subclase.coche import Coche
+
+class Motocicleta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        super().__init__(color, ruedas, velocidad, cilindrada)
